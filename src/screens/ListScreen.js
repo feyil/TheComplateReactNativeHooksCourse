ebef9a664312f39716b41ab5e4fa29de6675205d
Furkan Emre <furkan54emre@gmail.com>
2023-08-14 23:25:52 +0300

    Basic button implementation

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -5,25 +5,25 @@ const ListScreen = () => {
     const friends = [
         {
             name: "Friend #1",
-            age:  "Age 20"
+            age: "Age 20"
         },
         {
             name: "Friend #2",
-            age:  "Age 45"
+            age: "Age 45"
         },
         {
             name: "Friend #3",
-            age:  "Age 32"
+            age: "Age 32"
         },
         {
             name: "Friend #4",
-            age:  "Age 27"
+            age: "Age 27"
         },
         {
             name: "Friend #5",
-            age:  "Age 53"
+            age: "Age 53"
         },
-      
+
     ];
 
     return (
