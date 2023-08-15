6226f77447822de1c9778d239630c0e98ee63500
Furkan Emre <furkan54emre@gmail.com>
2023-08-15 12:04:57 +0300

    Showing images

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,8 +1,11 @@
 import React from "react";
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, Image} from 'react-native'
 
-const ImageDetail = () => {
-    return <Text>Show images of forest</Text>
+const ImageDetail = (props) => {
+    return <View>
+        <Image source={require('../../assets/beach.jpg')}/>
+        <Text>{props.title}</Text>
+    </View>
 }
 
 const styles = StyleSheet.create({});
